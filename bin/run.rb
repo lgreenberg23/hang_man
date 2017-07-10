4a8af27688d30a0377e1eb17708c3e5b4585ede9
zwilderrr <zwilder@gmail.com>
2017-07-10 18:01:16 -0400

    Added features

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,7 @@
+require_relative '../app/models/hangman_pic.rb'
+require_relative '../app/models/dictionary.rb'
 require_relative '../app/models/game.rb'
 require_relative '../app/models/user.rb'
-require_relative '../app/models/dictionary.rb'
 require 'pry'
 
 
